89bf16e8f190a37db75f65a2c03199f7aea01851
guillermooo <guilan70@hotmail.com>
2011-10-19 23:15:38 +0200

    change file name

--- sublime_hg.py
+++ sublime_hg.py
@@ -17,7 +17,7 @@ from hg_commands import HG_COMMANDS_AND_SHORT_HELP
 VERSION = '11.10.18b'
 
 
-CMD_LINE_SYNTAX = 'Packages/SublimeHg/Support/SublimeHg Command Line.tmLanguage'
+CMD_LINE_SYNTAX = 'Packages/SublimeHg/Support/SublimeHg Command Line.hidden-tmLanguage'
 
 ###############################################################################
 # Globals
